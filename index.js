7c04a2b11353a9937b171ec5dfcfd1bc14039cb4
Eemil Lähteenmäki <49359558+Eepu02@users.noreply.github.com>
2023-02-27 16:44:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// const menuBtn = document.querySelector('#menu-btn');
+
+// menuBtn.addEventListener('onClick', () => {
+    //     console.log("clicked");
+    //     menu.classList.toggle('visible');
+    // });
+    
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('visible');
+}
